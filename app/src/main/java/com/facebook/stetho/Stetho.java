f0648083feab25d9d591c048298be8206f40d6f4
chengong1 <chengong1@xiaomi.com>
2019-03-07 11:20:06 +0800

    Initial commit

---
+++ app/src/main/java/com/facebook/stetho/Stetho.java
@@ -0,0 +1,13 @@
+package com.facebook.stetho;
+
+import android.content.Context;
+
+/**
+ * Created by KyleCe on 2019/3/7
+ */
+public class Stetho {
+
+    public static void initializeWithDefaults(final Context context) {
+        // do nothing
+    }
+}
