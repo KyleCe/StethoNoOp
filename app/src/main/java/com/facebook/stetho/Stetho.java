19489fd71bfad16527a610842a5f7a7ab8977397
chengong1 <chengong1@xiaomi.com>
2019-03-07 17:53:10 +0800

    [add] class,jar

--- app/src/main/java/com/facebook/stetho/Stetho.java
+++ app/src/main/java/com/facebook/stetho/Stetho.java
@@ -1,13 +1,13 @@
 package com.facebook.stetho;
 
-import android.content.Context;
+import java.lang.Object;
 
 /**
  * Created by KyleCe on 2019/3/7
  */
 public class Stetho {
 
-    public static void initializeWithDefaults(final Context context) {
+    public static void initializeWithDefaults(final Object object) {
         // do nothing
     }
 }
